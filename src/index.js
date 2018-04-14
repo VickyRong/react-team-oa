75518e067b6922efd44a5af689c4fc961b65d7ae
huyanrong <515020842@qq.com>
2018-04-15 00:48:28 +0800

    搭建基本布局

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './App';
+import Home from './pages/home/home.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Home />,
+    document.getElementById('root')
+);
 registerServiceWorker();
