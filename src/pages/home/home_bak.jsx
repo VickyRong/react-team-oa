48a6aa223847e4432987eee021f63324287d0373
huyanrong <515020842@qq.com>
2018-04-16 23:59:49 +0800

    yy

--- src/pages/home/home_bak.jsx
+++ src/pages/home/home_bak.jsx
@@ -22,8 +22,7 @@ class Home extends Component {
             style={{ height: '100%' }}
           >
             <SubMenu key="sub1" title={<span><Icon type="user" />组员管理</span>}>
-              <Menu.Item key="1">
-                {/* <Link to="/memberList"> 组员列表 </Link> */}
+              <Menu.Item key="1">组员列表
               </Menu.Item>
               <Menu.Item key="2">添加组员
               </Menu.Item>
@@ -40,12 +39,12 @@ class Home extends Component {
         </Sider>
         <Layout className="layout-r" style={{ padding: '0 24px 24px' }}>
           <Breadcrumb style={{ margin: '12px 0' }}>
-            <Breadcrumb.Item>首页</Breadcrumb.Item>
-            <Breadcrumb.Item>组员管理</Breadcrumb.Item>
-            <Breadcrumb.Item>组员列表</Breadcrumb.Item>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
           </Breadcrumb>
           <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
-            内容1
+            Content
           </Content>
         </Layout>
       </Layout>
--- src/pages/member/list.jsx
+++ src/pages/member/list.jsx
@@ -1,1 +1,10 @@
 import React, { Component } from 'react';
+
+class MemberList extends Component {
+    render(){
+        return (
+            <h1>组员列表页</h1>
+        )
+    }
+}
+export default MemberList;
